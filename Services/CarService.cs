b53f6d72fcfb72b7468e939a2369caaf98eb2b45
mayorovmp <mayorovmp@yandex.ru>
2020-04-24 22:00:37 +0500

    remove unused code

--- Services/CarService.cs
+++ Services/CarService.cs
@@ -28,15 +28,6 @@ namespace GrandElementApi.Services
                 car.RowStatus = RowStatus.Removed;
                 await db.SaveChangesAsync();
             }
-            //using (var conn = _connectionService.GetConnection())
-            //{
-            //    conn.Open();
-            //    using (var cmd = new NpgsqlCommand("update cars set row_status=1 where id = @id", conn))
-            //    {
-            //        cmd.Parameters.Add(new NpgsqlParameter<int>("id", carId));
-            //        var affectedRows = await cmd.ExecuteNonQueryAsync();
-            //    }
-            //}
         }
         public async Task<Car> AddCarAsync(Car car) {
             using (var db = new ApplicationContext())
@@ -46,28 +37,6 @@ namespace GrandElementApi.Services
                 await db.SaveChangesAsync();
                 return car;
             }
-            //using (var conn = _connectionService.GetOpenedConnection())
-            //{
-            //    using (var cmd = new NpgsqlCommand("insert into cars(owner, contacts, comments, car_category_id, freight_price, unit, vat) values (@owner, @contacts, @comments, @category_id, @freight_price, @unit, @vat) RETURNING id", conn))
-            //    {
-            //        cmd.Parameters.AddRange(new[] {
-            //            car.Owner == null ? new NpgsqlParameter("owner", DBNull.Value) : new NpgsqlParameter<string>("owner", car.Owner),
-            //            car.Contacts == null ? new NpgsqlParameter("contacts", DBNull.Value) : new NpgsqlParameter<string>("contacts", car.Contacts),
-            //            car.Comments == null ? new NpgsqlParameter("comments", DBNull.Value) : new NpgsqlParameter<string>("comments", car.Comments),
-            //            car.CarCategory == null ? new NpgsqlParameter("category_id", DBNull.Value) : new NpgsqlParameter("category_id", car.CarCategory.Id),
-            //            car.FreightPrice == null ? new NpgsqlParameter("freight_price", DBNull.Value) : new NpgsqlParameter("freight_price", car.FreightPrice),
-            //            car.Unit == null ? new NpgsqlParameter("unit", DBNull.Value) : new NpgsqlParameter("unit", car.Unit),
-            //            new NpgsqlParameter("vat", car.VAT ? 1 : 0),
-            //        });
-
-            //        var reader = await cmd.ExecuteReaderAsync();
-            //        if (reader.Read())
-            //        {
-            //            car.Id = reader.GetInt32(0);
-            //        }
-            //        return car;
-            //    }
-            //}
         }
         public async Task<Car> EditCarAsync(Car car)
         {
@@ -88,112 +57,14 @@ namespace GrandElementApi.Services
                 await db.SaveChangesAsync();
             }
             return car;
-
-//            if (!car.Id.HasValue)
-//                throw new ArgumentException("Идентификатор записи пустой");
-//            int id = car.Id.GetValueOrDefault();
-
-//            if (car.Owner == null)
-//                throw new ArgumentException("Укажите владельца");
-
-
-//            using (var conn = _connectionService.GetConnection())
-//            {
-//                conn.Open();
-//                using (var cmd = new NpgsqlCommand(@"
-//update cars set owner = @owner, contacts=@contacts, comments=@comments, car_category_id=@category_id, freight_price=@freight_price, 
-//unit=@unit,
-//vat=@vat
-//where id=@id 
-//RETURNING id, owner, state_number, contacts, comments, freight_price", conn))
-//                {
-//                    cmd.Parameters.AddRange(new[] {
-//                        new NpgsqlParameter<int>("id", id),
-//                        new NpgsqlParameter<string>("owner", car.Owner),
-//                        new NpgsqlParameter<string>("contacts", car.Contacts),
-//                        new NpgsqlParameter<string>("comments", car.Comments),
-//                        car.FreightPrice == null ? new NpgsqlParameter("freight_price", DBNull.Value) : new NpgsqlParameter("freight_price", car.FreightPrice),
-//                        car.CarCategory == null ? new NpgsqlParameter("category_id", DBNull.Value) : new NpgsqlParameter("category_id", car.CarCategory.Id),
-//                        car.Unit == null ? new NpgsqlParameter("unit", DBNull.Value) : new NpgsqlParameter("unit", car.Unit),
-//                        car.VAT ? new NpgsqlParameter("vat", DBNull.Value) : new NpgsqlParameter("vat", car.VAT ? 1 : 0),
-//                    });
-
-//                    var reader = await cmd.ExecuteReaderAsync();
-
-//                    return await GetCarAsync(id);
-//                }
-//            }
         }
 
-        public async Task<Car> GetCarAsync(int id)
-        {
-            throw new NotImplementedException();
-//            using (var conn = _connectionService.GetConnection())
-//            {
-//                conn.Open();
-//                using (var cmd = new NpgsqlCommand(@"
-//select c.id, c.owner, c.contacts, c.comments, cc.id, cc.name, c.freight_price, c.vat, c.unit
-//from cars c left join car_categories cc on c.car_category_id = cc.id
-//where c.id = @id and c.row_status=0
-//order by c.created desc
-//", conn))
-//                {
-//                    cmd.Parameters.AddWithValue("id", NpgsqlTypes.NpgsqlDbType.Integer, id);
-//                    var reader = await cmd.ExecuteReaderAsync();
-//                    if (reader.HasRows)
-//                    {
-//                        reader.Read();
-//                        return new Car()
-//                        {
-//                            Id = reader.GetInt32(0),
-//                            Owner = reader.SafeGetString(1),
-//                            Contacts = reader.SafeGetString(2),
-//                            Comments = reader.SafeGetString(3),
-//                            CarCategory = reader.IsDBNull(4) ? null : new CarCategory() { Id = reader.GetInt32(4), Name = reader.SafeGetString(5) },
-//                            FreightPrice = reader.SafeGetDecimal(6),
-//                            VAT = reader.SafeGetDecimal(7).GetValueOrDefault(1) == 1 ? true : false,
-//                            Unit = reader.SafeGetString(8)
-//                        };
-//                    }
-//                    else {
-//                        throw new ArgumentException("Идентификатор не найден");
-//                    }
-//                }
-//            }
-        }
 
         public async Task<List<Car>> AllCarsAsync()
         {
             using (var db = new ApplicationContext()) {
                 return await db.Cars.Where(x => x.RowStatus == RowStatus.Active).Include(x=>x.CarCategory).ToListAsync();
             }
-//            using (var conn = _connectionService.GetConnection())
-//            {
-//                conn.Open();
-//                using (var cmd = new NpgsqlCommand(@"
-//select c.id, c.owner, c.contacts, c.comments, cc.id, cc.name, c.freight_price, c.vat, c.unit
-//from cars c left join car_categories cc on c.car_category_id = cc.id where c.row_status=0
-//order by c.owner", conn))
-//                {
-//                    var reader = await cmd.ExecuteReaderAsync();
-//                    var data = new List<Car>();
-//                    while (reader.Read())
-//                    {
-//                        data.Add(new Car()
-//                        {
-//                            Id = reader.GetInt32(0),
-//                            Owner = reader.SafeGetString(1),
-//                            Contacts = reader.SafeGetString(2),
-//                            Comments = reader.SafeGetString(3),
-//                            CarCategory = reader.IsDBNull(4) ? null : new CarCategory() { Id = reader.GetInt32(4), Name = reader.SafeGetString(5) },
-//                            FreightPrice = reader.SafeGetDecimal(6),
-//                            VAT = reader.SafeGetDecimal(7).GetValueOrDefault(1) == 1 ? true : false,
-//                            Unit = reader.SafeGetString(8)
-//                        }) ;
-//                    }
-//                    return data;
-//                }
-//            }
         }
     }
 }
--- Services/SupplierService.cs
+++ Services/SupplierService.cs
@@ -31,56 +31,8 @@ namespace GrandElementApi.Services
                     .First(x => x.Id == supplier.Id);
                 return supplier;
             }
-            //using (var tran = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
-            //using (var conn = _connectionService.GetOpenedConnection())
-            //{
-            //    using (var cmd = new NpgsqlCommand("insert into suppliers(legal_entity, address, name, vat) values (@LegalEntity, @Address, @Name, @vat) RETURNING id", conn))
-            //    {
-            //        cmd.Parameters.AddRange(new[] { 
-            //            new NpgsqlParameter<string>("LegalEntity", supplier.LegalEntity),
-            //            new NpgsqlParameter<string>("Address", supplier.Address),
-            //            new NpgsqlParameter<string>("Name", supplier.Name),
-            //            new NpgsqlParameter("vat", supplier.VAT ? 1 : 0),
-            //        });
-                    
-
-            //        using (var reader = await cmd.ExecuteReaderAsync())
-            //        {
-            //            if (reader.Read())
-            //            {
-            //                supplier.Id = reader.GetInt32(0);
-            //            }
-            //        }
-            //    }
-                
-            //    foreach (var prod in supplier.Products)
-            //    {
-            //        AddSupplierProduct(conn, prod, supplier.Id.Value);
-            //    }
-            //    tran.Complete();
-            //}
-            //return supplier;
         }
 
-        private void AddSupplierProduct(NpgsqlConnection conn, Product prod, int supplierId)
-        {
-            throw new NotImplementedException();
-            //            if (!prod.Id.HasValue)
-            //            {
-            //                throw new Exception("Идентификатор товара не может быть пустым");
-            //            }
-            //            var cmd = new NpgsqlCommand(@"
-            //insert into supplier_product(supplier_id, product_id, price) values(@supId, @prodId, @price) 
-            //RETURNING supplier_id, product_id, price", conn);
-            //            {
-            //                cmd.Parameters.AddRange(new[] {
-            //                        new NpgsqlParameter<int>("prodId", prod.Id.Value),
-            //                        new NpgsqlParameter<int>("supId", supplierId),
-            //                        prod.Price == null ? new NpgsqlParameter("price", DBNull.Value) : new NpgsqlParameter("price", prod.Price)
-            //                    });
-            //                cmd.ExecuteNonQuery();
-            //            }
-        }
         public async Task<List<Supplier>> AllSuppliersAsync()
         {
             using (var db = new ApplicationContext()) {
@@ -99,120 +51,8 @@ namespace GrandElementApi.Services
                         Updated = x.Updated
                     }).ToListAsync();
             }
-
-//            List<Supplier> suppliers = new List<Supplier>();
-
-//            List<SupplierProductRow> rows = new List<SupplierProductRow>();
-//            using (var conn = _connectionService.GetConnection())
-//            {
-//                conn.Open();
-//                using (var cmd = new NpgsqlCommand(@"
-//select s.id, s.name, s.legal_entity, s.address, sp.price, sp.product_id, p.name, s.vat
-//from suppliers s
-//    left join supplier_product sp on s.id = sp.supplier_id and sp.row_status = 0
-//    left join products p on p.id = sp.product_id
-//where s.row_status=0
-//order by s.created desc
-//", conn))
-//                {
-//                    using var reader = await cmd.ExecuteReaderAsync();
-//                    while (reader.Read())
-//                    {
-//                        rows.Add(new SupplierProductRow()
-//                        {
-//                            SupplierId = reader.GetInt32(0),
-//                            SupplierName = reader.SafeGetString(1),
-//                            LegalName = reader.SafeGetString(2),
-//                            Address = reader.SafeGetString(3),
-//                            Price = reader.SafeGetDecimal(4),
-//                            ProductId = reader.SafeGetInt32(5),
-//                            ProductName = reader.SafeGetString(6),
-//                            VAT = reader.SafeGetDecimal(7).GetValueOrDefault(1) == 1 ? true : false,
-//                        });
-//                    }
-//                }
-//            }
-//            var groups = rows.GroupBy(x=>x.SupplierId);
-//            foreach(var group in groups)
-//            {
-//                var supplier = new Supplier()
-//                {
-//                    Id = group.Key,
-//                    Name = group.First().SupplierName,
-//                    LegalEntity = group.First().LegalName,
-//                    Address = group.First().Address,
-//                    Products = new List<Product>(),
-//                    VAT = group.First().VAT.Value
-                    
-//                };
-//                foreach (var row in group)
-//                {
-//                    if (row.ProductId.HasValue)
-//                    {
-//                        supplier.Products.Add(new Product() { Id = row.ProductId, Name = row.ProductName, Price = row.Price });
-//                    }
-//                }
-//                suppliers.Add(supplier);
-//            }
-//            return suppliers;
         }
 
-        public async Task<Supplier> GetSupplier(int id)
-        {
-
-            throw new NotImplementedException();
-            //            List<SupplierProductRow> rows = new List<SupplierProductRow>();
-            //            using (var conn = _connectionService.GetOpenedConnection())
-            //            {
-            //                using (var cmd = new NpgsqlCommand(@"
-            //select s.id, s.name, s.legal_entity, s.address, sp.price, sp.product_id, p.name, s.vat
-            //from suppliers s
-            //    left join supplier_product sp on s.id = sp.supplier_id and sp.row_status=0
-            //    left join products p on p.id = sp.product_id
-            //where s.id = @id
-            //and s.row_status=0
-            //order by s.id desc
-            //", conn))
-            //                {
-            //                    cmd.Parameters.AddWithValue("id", NpgsqlDbType.Integer, id);
-            //                    using (var reader = await cmd.ExecuteReaderAsync())
-            //                        while (reader.Read())
-            //                        {
-            //                            rows.Add(new SupplierProductRow()
-            //                            {
-            //                                SupplierId = reader.GetInt32(0),
-            //                                SupplierName = reader.SafeGetString(1),
-            //                                LegalName = reader.SafeGetString(2),
-            //                                Address = reader.SafeGetString(3),
-            //                                Price = reader.SafeGetDecimal(4),
-            //                                ProductId = reader.SafeGetInt32(5),
-            //                                ProductName = reader.SafeGetString(6),
-            //                                VAT = reader.SafeGetDecimal(7).GetValueOrDefault(1) == 1 ? true : false,
-            //                            });
-            //                        }
-            //                }
-            //            }
-            //            if (rows.Count == 0)
-            //                throw new ArgumentException("Идентификатор не найден");
-            //            var groups = rows.GroupBy(x => x.SupplierId);
-            //            var supplier = new Supplier()
-            //            {
-            //                Id = rows.First().SupplierId,
-            //                Name = rows.First().SupplierName,
-            //                LegalEntity = rows.First().LegalName,
-            //                Address = rows.First().Address,
-            //                Products = new List<Product>(),
-            //                VAT = rows.First().VAT.GetValueOrDefault(true)
-            //            };
-            //            foreach (var row in rows)
-            //            {
-            //                if (row.ProductId.HasValue)
-            //                {
-            //                    supplier.Products.Add(new Product() { Id = row.ProductId, Name = row.ProductName, Price = row.Price });
-            //                }
-            //            }
-            //            return supplier;
-        }
         public async Task DeleteSupplierAsync(int id)
         {
             using (var db = new ApplicationContext()) {
@@ -224,48 +64,6 @@ namespace GrandElementApi.Services
                     .Where(x => x.SupplierId == supplier.Id).ToList().ForEach(x=>x.RowStatus = RowStatus.Removed);
                 await db.SaveChangesAsync();
             }
-            //using (var conn = _connectionService.GetOpenedConnection())
-            //{
-            //    using (var cmd = new NpgsqlCommand("update suppliers set row_status = 1 where id = @id", conn))
-            //    {
-            //        cmd.Parameters.AddWithValue("id", id);
-            //        var affected = await cmd.ExecuteNonQueryAsync();
-            //    }
-            //}
-        }
-
-        private void DeleteSupplierProduct(NpgsqlConnection conn, int supplierId)
-        {
-            using (var cmd = new NpgsqlCommand(@"
-update supplier_product set 
-row_status=1
-where supplier_id = @id", conn))
-            {
-                cmd.Parameters.Add(new NpgsqlParameter<int>("id", supplierId));
-                var updatedRowsCnt = cmd.ExecuteNonQuery();
-            }
-        }
-
-        private void UpdateSupplier(NpgsqlConnection conn, Supplier supplier) {
-
-            throw new NotImplementedException();
-            //            using (var cmd = new NpgsqlCommand(@"
-            //update suppliers set 
-            //legal_entity=@legal_entity, 
-            //address=@address,
-            //name=@name,
-            //vat=@vat
-            //where id = @id returning id, name, legal_entity, address", conn))
-            //            {
-            //                cmd.Parameters.AddRange(new[] {
-            //                    new NpgsqlParameter<int>("id", supplier.Id.Value),
-            //                    new NpgsqlParameter<string>("legal_entity", supplier.LegalEntity),
-            //                    new NpgsqlParameter<string>("name", supplier.Name),
-            //                    new NpgsqlParameter<string>("address", supplier.Address),
-            //                    new NpgsqlParameter("vat", supplier.VAT ? 1 : 0),
-            //                });
-            //                cmd.ExecuteNonQuery();
-            //            }
         }
 
         public async Task<Supplier> EditSupplierAsync(Supplier supplier)
@@ -286,27 +84,6 @@ where supplier_id = @id", conn))
                 await db.SaveChangesAsync();
                 return storedSupplier;
             }
-
-            //int id;
-            //if (supplier.Id.HasValue)
-            //    id = supplier.Id.Value;
-            //else
-            //    throw new ArgumentException("Идентификатор записи пустой");
-
-            //using (var tran = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
-            //using (var conn = _connectionService.GetOpenedConnection())
-            //{
-            //    DeleteSupplierProduct(conn, id);
-            //    UpdateSupplier(conn, supplier);
-
-            //    foreach (var prod in supplier.Products)
-            //    {
-            //        AddSupplierProduct(conn, prod, supplier.Id.Value);
-            //    }
-            //    tran.Complete();
-            //}
-
-            //return await GetSupplier(id);
         }
 
         public async Task<List<Supplier>> SuppliersByProductIdAsync(int productId)
@@ -329,73 +106,6 @@ where supplier_id = @id", conn))
                         Updated = x.Updated
                     }).ToListAsync();
             }
-            // throw new NotImplementedException();
-            //            List<Supplier> suppliers = new List<Supplier>();
-
-            //            List<SupplierProductRow> rows = new List<SupplierProductRow>();
-            //            using (var conn = _connectionService.GetConnection())
-            //            {
-            //                conn.Open();
-            //                using (var cmd = new NpgsqlCommand(@"
-            //select s.id, s.name, s.legal_entity, s.address, sp.price, sp.product_id, p.name, s.vat
-            //from suppliers s
-            //    left join supplier_product sp on s.id = sp.supplier_id and sp.row_status = 0
-            //    left join products p on p.id = sp.product_id
-            //where s.row_status=0 and p.id = @productId
-            //", conn))
-            //                {
-            //                    cmd.Parameters.Add(new NpgsqlParameter<Int32>("productId", productId));
-            //                    using var reader = await cmd.ExecuteReaderAsync();
-            //                    while (reader.Read())
-            //                    {
-            //                        rows.Add(new SupplierProductRow()
-            //                        {
-            //                            SupplierId = reader.GetInt32(0),
-            //                            SupplierName = reader.SafeGetString(1),
-            //                            LegalName = reader.SafeGetString(2),
-            //                            Address = reader.SafeGetString(3),
-            //                            Price = reader.SafeGetDecimal(4),
-            //                            ProductId = reader.SafeGetInt32(5),
-            //                            ProductName = reader.SafeGetString(6),
-            //                            VAT = reader.SafeGetDecimal(7).GetValueOrDefault(1) == 1 ? true : false,
-            //                        });
-            //                    }
-            //                }
-            //            }
-            //            var groups = rows.GroupBy(x => x.SupplierId);
-            //            foreach (var group in groups)
-            //            {
-            //                var supplier = new Supplier()
-            //                {
-            //                    Id = group.Key,
-            //                    Name = group.First().SupplierName,
-            //                    LegalEntity = group.First().LegalName,
-            //                    Address = group.First().Address,
-            //                    Products = new List<Product>(),
-            //                    VAT = group.First().VAT.GetValueOrDefault(true)
-
-            //                };
-            //                foreach (var row in group)
-            //                {
-            //                    if (row.ProductId.HasValue)
-            //                    {
-            //                        supplier.Products.Add(new Product() { Id = row.ProductId, Name = row.ProductName, Price = row.Price });
-            //                    }
-            //                }
-            //                suppliers.Add(supplier);
-            //            }
-            //            return suppliers;
-        }
-
-        private class SupplierProductRow {
-            public int SupplierId { get; set; }
-            public string SupplierName { get; set; }
-            public string LegalName { get; set; }
-            public string Address { get; set; }
-            public int? ProductId { get; set; }
-            public string ProductName { get; set; }
-            public decimal? Price { get; set; }
-            public bool? VAT { get; set; }
         }
     }
 }

40461a3d9df45cf8c05d7112988930fa1577f0d0
mayorovmp <mayorovmp@yandex.ru>
2020-04-25 22:20:31 +0500

    configure swagger

--- Startup.cs
+++ Startup.cs
@@ -76,12 +76,16 @@ namespace GrandElementApi
         {
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "api/swagger/{documentname}/swagger.json";
+            });
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Grand Element API V1");
+                c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "Grand Element API V1");
+                c.RoutePrefix = "api/swagger";
             });
             app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 

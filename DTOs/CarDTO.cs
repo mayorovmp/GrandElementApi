8cf223d8362b67ca16dffe4406e4ae3836939ab9
Макс Майоров <majorovmp@it2g.ru>
2021-06-17 05:40:37 +0300

    Удалил закомменченные поля

--- DTOs/CarDTO.cs
+++ DTOs/CarDTO.cs
@@ -1,10 +1,4 @@
-﻿using GrandElementApi.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace GrandElementApi.DTOs
+﻿namespace GrandElementApi.DTOs
 {
     public class CarDTO
     {
@@ -12,10 +6,8 @@ namespace GrandElementApi.DTOs
         public string Owner { get; set; }
         public string Contacts { get; set; }
         public string Comments { get; set; }
-        //public decimal? FreightPrice { get; set; }
         public string Unit { get; set; }
         public bool Vat { get; set; }
-
         public virtual CarCategoryDTO CarCategory { get; set; }
     }
 
@@ -24,10 +16,8 @@ namespace GrandElementApi.DTOs
         public string Owner { get; set; }
         public string Contacts { get; set; }
         public string Comments { get; set; }
-        //public decimal? FreightPrice { get; set; }
         public string Unit { get; set; }
         public bool Vat { get; set; }
-
         public virtual CarCategoryDTO CarCategory { get; set; }
     }
 }
